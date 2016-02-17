28a654aadb3f50f55a21959d8d22843a0779b19e
Oscar Roa <oshardy3@gmail.com>
2016-02-17 00:08:03 -0600

    Add pagination

--- api_cats/settings.py
+++ api_cats/settings.py
@@ -133,5 +133,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 25
 }
