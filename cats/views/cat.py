c81e98b9e20d78648d4b40fb3c130c6e1f147d4c
Oscar Roa <oshardy3@gmail.com>
2016-02-17 16:24:16 -0600

    Add query and search params in cats endpoints

--- cats/views/cat.py
+++ cats/views/cat.py
@@ -1,5 +1,5 @@
 from cats.models import Cat
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from cats.serializers.cat import CatSerializer
 
 
@@ -10,6 +10,27 @@ class CatList(generics.ListCreateAPIView):
     )
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    # Search in endpoint
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('$breed__name', '$name',
+                     '$owner__username')
+
+    def get_queryset(self):
+        queryset = Cat.objects.all()
+        name = self.request.query_params.get('name', None)
+        breed = self.request.query_params.get('breed', None)
+        owner = self.request.query_params.get('owner', None)
+
+        if name is not None:
+            queryset = queryset.filter(name=name)
+
+        if breed is not None:
+            queryset = queryset.filter(breed=breed)
+
+        if owner is not None:
+            queryset = queryset.filter(owner=owner)
+
+        return queryset
 
 
 class CatDetail(generics.RetrieveUpdateDestroyAPIView):
