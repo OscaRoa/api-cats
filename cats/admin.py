ea65caa54b670549ca21a61330698823e40ce55a
Oscar Roa <oshardy3@gmail.com>
2016-02-16 23:19:57 -0600

    Add models

--- cats/admin.py
+++ cats/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import (
+    Cat,
+    Breed
+)
 
-# Register your models here.
+
+@admin.register(Breed)
+class BreedAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('name',)
+        }),
+    )
+
+    list_display = ('id', 'name')
+
+
+@admin.register(Cat)
+class CatAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'breed', 'age', 'photo', 'owner')
+        }),
+    )
+
+    list_display = ('id', 'name', 'breed', 'owner')
--- cats/models.py
+++ cats/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Breed(models.Model):
+    """Docstring for Cat Breed model"""
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.ForeignKey(Breed)
+    age = models.PositiveIntegerField(default=0)
+    photo = models.URLField(blank=True, null=True)
+
+    owner = models.ForeignKey(User)
+
+    def __str__(self):
+        return "{name} | Breed {breed} | Owner {owner}".format(
+            name=self.name,
+            breed=self.breed.name,
+            owner=self.owner.first_name,
+        )
